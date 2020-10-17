a53072727c5bf39c29194665fa976fb6cce58e22
jebadi25 <https://jebadi25@bitbucket.org/jebadi25/proyecto_1.git>
2020-10-16 21:49:01 -0600

    publicando blog1

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+
+
+// setting
+app.set('port', process.env.PORT || 4200);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+//importando rutas
+const indexRouter = require('./routes/routes');
+const { urlencoded } = require('express');
+
+
+//middlewear
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+//rutas
+app.use('/', indexRouter);
+
+
+
+app.listen(app.get('port'), function(){
+    console.log(`server on port ${app.get('port')}`);
+});
