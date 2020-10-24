c9dae7ff80b78aac5cc3a1982508f8eeb21db61b
jebadi25 <https://jebadi25@bitbucket.org/jebadi25/proyecto_1.git>
2020-10-23 18:57:43 -0600

    agregando maps me

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,13 @@ router.get('/', function(req, res){
     res.render('index')
 });
 
+router.get('/mobil/sofware/mapme', function(req, res){
+    res.render('mobil/sofware/mapme')
+});
+
+router.get('/politica/politica', function(req, res){
+    res.render('politica/politica')
+});
 
 
 
